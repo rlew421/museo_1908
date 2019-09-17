df01fcbac23877d3ed0b45494e2a3efa3c9753b1
Rachel Lew <rlew07@gmail.com>
2019-09-17 11:41:26 -0600

    Adds attempts at artists with multiple photographs and photographs taken by artist from country

--- lib/curator.rb
+++ lib/curator.rb
@@ -31,4 +31,38 @@ class Curator
       photograph.artist_id == artist.id
     end
   end
+
+  def artists_with_multiple_photographs
+    # find all artists whose id matches the artist_id
+    # of more than one photograph
+    # call on artist id and return an array of those artist objects
+    # use find_photographs_by_artist method?
+
+    photograph_artist_ids = @photographs.map do |photograph|
+      photograph.artist_id
+    end
+
+    # artist_ids = @artists.map do |artist|
+    #   artist.id
+    # end
+    # require 'pry'; binding.pry
+    # @artists.find_all do |artist|
+    #   (artist.id == photograph.artist_id) > 1
+    # end
+  end
+
+  def photographs_taken_by_artist_from(country)
+    # find all photographs whose artist id matches the id of
+    # the artist whose hash contains the string
+    artist_countries = @artists.map do |artist|
+      artist.country
+    end
+    photograph_artist_ids = @photographs.map do |photograph|
+        photograph.artist_id
+    end
+
+    @photographs.find_all do |photograph|
+      photograph.artist_id
+    end
+  end
 end
