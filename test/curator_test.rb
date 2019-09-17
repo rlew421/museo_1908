9fa8d0549de1d9dca1914b83347ce55fc250ab8e
Rachel Lew <rlew07@gmail.com>
2019-09-17 09:42:53 -0600

    Adds tests for methods to add artists and photographs and find them by id

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -7,6 +7,32 @@ require './lib/curator'
 class CuratorTest < Minitest::Test
   def setup
     @curator = Curator.new
+    @photo_1 = Photograph.new({
+         id: "1",
+         name: "Rue Mouffetard, Paris (Boy with Bottles)",
+         artist_id: "1",
+         year: "1954"
+    })
+    @photo_2 = Photograph.new({
+         id: "2",
+         name: "Moonrise, Hernandez",
+         artist_id: "2",
+         year: "1941"
+    })
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
   end
 
   def test_it_exists
@@ -15,5 +41,30 @@ class CuratorTest < Minitest::Test
 
   def test_it_starts_with_empty_array_of_photographs
     assert_equal [], @curator.photographs
+  end
+
+  def test_it_can_add_photographs
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_it_starts_with_empty_array_of_artists
+    assert_equal [], @curator.artists
+  end
+
+  def test_it_can_add_artists
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    assert_equal [@artist_1, @artist_2], @curator.artists
+  end
+
+  def test_it_can_find_artist_by_id
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    assert_equal @artist_1, @curator.find_artist_by_id("1")
+    assert_equal @photo_2, @curator.find_photograph_by_id("2")
   end
 end
